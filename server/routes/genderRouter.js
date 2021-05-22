dc048829196c3cede2b9642645269e02b320567d
Harovinkin <kharovinkin@gmail.com>
2021-05-22 14:37:26 +0300

    add changeRole to directories

--- server/routes/genderRouter.js
+++ server/routes/genderRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const genderController = require('../controllers/genderController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 const router = new Router()
 
-router.post('/', genderController.create)
-router.get('/', genderController.getAll) 
+router.post('/', checkRole(2), genderController.create)
+router.get('/', checkRole(2), genderController.getAll) 
 
 module.exports = router
